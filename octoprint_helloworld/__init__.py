6020b9f211d011f74cc7654f1bf9afbdd71616ea
cmbergquist@gmail.com <cmbergquist@gmail.com>
2020-07-30 17:32:22 -0500

    Log all events, a few with special formatting. Proves I get and process them.

--- octoprint_helloworld/__init__.py
+++ octoprint_helloworld/__init__.py
@@ -4,17 +4,20 @@
 #
 # 2020-07-29 - Initial version.
 # 2020-07-30 - Comments, header, test GitHub.
+#              Added first version of Event handler.
 #
 from __future__ import absolute_import, unicode_literals
 
+import octoprint.events
 import octoprint.plugin
 
-class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
+class HelloWorldPlugin(octoprint.plugin.EventHandlerPlugin,
+                       octoprint.plugin.StartupPlugin,
                        octoprint.plugin.TemplatePlugin,
                        octoprint.plugin.SettingsPlugin,
                        octoprint.plugin.AssetPlugin):
     def on_after_startup(self):
-        self._logger.info("Change - Hello World! (more: %s)" % self._settings.get(["url"]))
+        self._logger.info("URL: %s" % self._settings.get(["url"]))
 
     def get_settings_defaults(self):
         return dict(url="https://en.wikipedia.org/wiki/Hello_world")
@@ -29,7 +32,19 @@ class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
         return dict(
             js=["js/helloworld.js"]
         )
+    
+    def on_event(self, event, payload):
+        if event == octoprint.events.Events.UPLOAD:
+            self._logger.info("File Uploaded: %s" % payload["file"])
 
-__plugin_name__ = "Change - Hello World"
+        elif event == octoprint.events.Events.CLIENT_OPENED:
+            self._logger.info("File Uploaded: %s" % payload["remoteAddress"])
+
+        else:
+            self._logger.info("Unhandled Event: %s" % event)
+
+
+
+__plugin_name__ = "CNC Camera"
 __plugin_pythoncompat__ = ">=2.7,<4"
 __plugin_implementation__ = HelloWorldPlugin()
--- setup.py
+++ setup.py
@@ -1,67 +1,25 @@
 # coding=utf-8
-
-########################################################################################################################
-### Do not forget to adjust the following variables to your own plugin.
-
-# The plugin's identifier, has to be unique
-plugin_identifier = "helloworld"
-
-# The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_helloworld"
-
-# The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
-# plugin module
-plugin_name = "OctoPrint-Helloworld"
-
-# The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.0.2"
-
-# The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
-# module
-plugin_description = "A test OctoPrint plugin."
-
-# The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "You"
-
-# The plugin's author's mail address.
-plugin_author_email = "cmbergquist@gmail.com"
-
-# The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/ChipBergquist/OctoPrint-Helloworld"
-
-# The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
-plugin_license = "AGPLv3"
-
-# Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
-
-### --------------------------------------------------------------------------------------------------------------------
-### More advanced options that you usually shouldn't have to touch follow after this point
-### --------------------------------------------------------------------------------------------------------------------
-
-# Additional package data to install for this plugin. The subfolders "templates", "static" and "translations" will
-# already be installed automatically if they exist. Note that if you add something here you'll also need to update
-# MANIFEST.in to match to ensure that python setup.py sdist produces a source distribution that contains all your
-# files. This is sadly due to how python's setup.py works, see also http://stackoverflow.com/a/14159430/2028598
-plugin_additional_data = []
-
-# Any additional python packages you need to install with your plugin that are not contained in <plugin_package>.*
-plugin_additional_packages = []
-
-# Any python packages within <plugin_package>.* you do NOT want to install with your plugin
-plugin_ignored_packages = []
-
-# Additional parameters for the call to setuptools.setup. If your plugin wants to register additional entry points,
-# define dependency links or other things like that, this is the place to go. Will be merged recursively with the
-# default setup parameters as provided by octoprint_setuptools.create_plugin_setup_parameters using
-# octoprint.util.dict_merge.
 #
-# Example:
-#     plugin_requires = ["someDependency==dev"]
-#     additional_setup_parameters = {"dependency_links": ["https://github.com/someUser/someRepo/archive/master.zip#egg=someDependency-dev"]}
+# Directions for setting up the plugin.
+#
+# 2020-07-29 - Initial version.
+# 2020-07-30 - Corrections and cleanup.
+#
+plugin_identifier = "helloworld"
+plugin_package = "octoprint_helloworld"
+plugin_name = "OctoPrint-Helloworld"
+plugin_version = "0.0.3"
+plugin_description = "A test OctoPrint plugin."
+plugin_author = "Chip Bergquist"
+plugin_author_email = "cmbergquist@gmail.com"
+plugin_url = "https://github.com/ChipBergquist/OctoPrint-Helloworld"
+plugin_license = "AGPLv3"
+plugin_requires = []
+plugin_additional_data = []
+plugin_additional_packages = []
+plugin_ignored_packages = []
 additional_setup_parameters = {}
 
-########################################################################################################################
 
 from setuptools import setup
 
