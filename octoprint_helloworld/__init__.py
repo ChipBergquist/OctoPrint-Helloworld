67981e681a7e26425f3dd25e30f4912482e93ae9
cmbergquist@gmail.com <cmbergquist@gmail.com>
2020-08-03 15:44:02 -0500

    Added simple API support.

--- octoprint_helloworld/__init__.py
+++ octoprint_helloworld/__init__.py
@@ -5,9 +5,11 @@
 # 2020-07-29 - Initial version.
 # 2020-07-30 - Comments, header, test GitHub.
 #              Added first version of Event handler.
+# 2020-08-03 - Simple API Support
 #
 from __future__ import absolute_import, unicode_literals
 
+import flask
 import json
 
 import octoprint.events
@@ -17,7 +19,8 @@ class HelloWorldPlugin(octoprint.plugin.EventHandlerPlugin,
                        octoprint.plugin.StartupPlugin,
                        octoprint.plugin.TemplatePlugin,
                        octoprint.plugin.SettingsPlugin,
-                       octoprint.plugin.AssetPlugin):
+                       octoprint.plugin.AssetPlugin,
+                       octoprint.plugin.SimpleApiPlugin):
     def on_after_startup(self):
         self._logger.info("URL: %s" % self._settings.get(["url"]))
 
@@ -41,6 +44,29 @@ class HelloWorldPlugin(octoprint.plugin.EventHandlerPlugin,
 
     def custom_atcommand_handler(self, comm, phase, command, parameters, tags=None, *args, **kwargs):
         self._logger.info('Command: {0}'.format(command))
+
+    def get_api_commands(self):
+        self._logger.info('get_api_commands')
+        return dict(
+            command1=[],
+            command2=["some_parameter"]
+        )
+
+    def on_api_command(self, command, data):
+        self._logger.info('on_api_command')
+        if command == "command1":
+            parameter = "unset"
+            if "parameter" in data:
+                parameter = "set"
+            self._logger.info("command1 called, parameter is {parameter}".format(**locals()))
+        elif command == "command2":
+            self._logger.info("command2 called, some_parameter is {some_parameter}".format(**data))
+        return flask.jsonify(who="you")
+
+    def on_api_get(self, request):
+        self._logger.info('on_api_get')
+        return flask.jsonify(foo="bar")
+        
 
 __plugin_name__ = "CNC Camera"
 __plugin_pythoncompat__ = ">=2.7,<4"
